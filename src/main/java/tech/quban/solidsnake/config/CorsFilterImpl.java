b58df8332cdcea823fb81772c63e73ec5dccb95b
Lihogub Oleg <oligli@yandex.ru>
2022-05-21 21:18:17 +0300

    added cors filter impl

---
+++ src/main/java/tech/quban/solidsnake/config/CorsFilterImpl.java
@@ -0,0 +1,34 @@
+package tech.quban.solidsnake.config;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component
+class CorsFilterImpl extends OncePerRequestFilter {
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws ServletException, IOException {
+        String origin = request.getHeader("Origin");
+        if (origin != null)
+            response.setHeader("Access-Control-Allow-Origin", origin);
+
+        response.setHeader("Access-Control-Allow-Credentials", "true");
+        response.setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        response.setHeader("Access-Control-Max-Age", "3600");
+        response.setHeader("Access-Control-Allow-Headers", "Origin, Content-Type, Authorization, refresh-token");
+        response.setHeader("Access-Control-Expose-Headers", "refresh-token");
+
+        if ("OPTIONS".equalsIgnoreCase(request.getMethod())) {
+            response.setStatus(HttpServletResponse.SC_OK);
+        } else {
+            filterChain.doFilter(request, response);
+        }
+    }
+}
